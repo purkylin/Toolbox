2691ca6017f4a043b8bd3f4d78f861b81711be7d
qujinshan <qujinshan@hujiang.com>
2016-09-30 07:26:51 +0800

    Initial Commit

---
+++ Toolbox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Toolbox
+//
+//  Created by Purkylin King on 2016/9/30.
+//  Copyright © 2016年 Purkylin King. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Toolbox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Toolbox
+//
+//  Created by Purkylin King on 2016/9/30.
+//  Copyright © 2016年 Purkylin King. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
