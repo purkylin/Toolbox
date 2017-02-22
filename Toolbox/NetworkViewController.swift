a5a1ae20ff56eaf0fe93ac3236da49bd7decb75d
qujinshan <qujinshan@hujiang.com>
2017-02-22 12:55:43 +0800

    加入dns设置

--- Toolbox/NetworkViewController.swift
+++ Toolbox/NetworkViewController.swift
@@ -10,29 +10,31 @@ import Cocoa
 
 class NetworkViewController: NSViewController {
 
-    @IBOutlet weak var dnsPopupButton: NSPopUpButton!
+    @IBOutlet weak var dnsComboBox: NSComboBox!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
         let defaults = UserDefaults.standard
         if let records = defaults.value(forKeyPath: "records") as? [String] {
-            dnsPopupButton.removeAllItems()
-            dnsPopupButton.addItems(withTitles: records)
+            dnsComboBox.removeAllItems()
+            dnsComboBox.addItems(withObjectValues: records)
             
             if let record = defaults.string(forKey: "currentDNS") {
                 if let index = records.index(of: record) {
-                    dnsPopupButton.selectItem(at: index)
+                    dnsComboBox.selectItem(at: index)
                 }
             }
         } else {
-            dnsPopupButton.removeAllItems()
-            dnsPopupButton.addItems(withTitles: ["默认", "8.8.8.8", "114.114.114.114"])
-            dnsPopupButton.selectItem(at: 0)
+            dnsComboBox.removeAllItems()
+            dnsComboBox.addItems(withObjectValues: ["默认", "8.8.8.8", "114.114.114.114"])
+            dnsComboBox.selectItem(at: 0)
         }
     }
     
     @IBAction func btnApplyClicked(_ sender: Any) {
-        if let title = dnsPopupButton.titleOfSelectedItem {
+        let title = dnsComboBox.stringValue
+        if title.characters.count > 0 {
             if title == "默认" {
                 SudoHelper.sharedInstance().runCommand("networksetup -setdnsservers Wi-Fi empty", completion: { (output, success) in
                     if !success {
@@ -41,6 +43,11 @@ class NetworkViewController: NSViewController {
                             alert.informativeText = output!
                             alert.beginSheetModal(for: self.view.window!, completionHandler: nil)
                         }
+                    } else {
+                        let defaults = UserDefaults.standard
+                        defaults.set(self.dnsComboBox.objectValues, forKey: "records")
+                        defaults.setValue(self.dnsComboBox.stringValue, forKey: "currentDNS")
+                        defaults.synchronize()
                     }
                 })
             } else {
@@ -51,15 +58,18 @@ class NetworkViewController: NSViewController {
                             alert.informativeText = output!
                             alert.beginSheetModal(for: self.view.window!, completionHandler: nil)
                         }
-
+                    } else {
+                        if self.dnsComboBox.indexOfItem(withObjectValue: self.dnsComboBox.stringValue) == NSNotFound {
+                            self.dnsComboBox.addItem(withObjectValue: self.dnsComboBox.stringValue)
+                            
+                            let defaults = UserDefaults.standard
+                            defaults.set(self.dnsComboBox.objectValues, forKey: "records")
+                            defaults.setValue(self.dnsComboBox.stringValue, forKey: "currentDNS")
+                            defaults.synchronize()
+                        }
                     }
                 })
             }
-            
-            let defaults = UserDefaults.standard
-            defaults.set(dnsPopupButton.itemTitles, forKey: "records")
-            defaults.setValue(dnsPopupButton.stringValue, forKey: "currentDNS")
-            defaults.synchronize()
         }
         
     }
